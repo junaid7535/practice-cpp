639d274df793d6fea3813c791db70d73ea0b061c
junaid7535 <zunaidkhan1492@gmail.com>
2025-07-17 16:37:13 +0530

    commit

--- temp.cpp
+++ temp.cpp
@@ -4,20 +4,8 @@
 #include<map>
 using namespace std;
 
-int result = INT_MAX;
-void solve(vector<int>nums,int sum1,int sum2,int index){
-    if(index == nums.size()){
-        result = min(result,abs(sum1-sum2));
-        return;
-    }
 
-    solve(nums,sum1+nums[index],sum2,index+1);
-    solve(nums,sum1,sum2 + nums[index],index+1);
-}
 
 int main(){
-    vector<int>nums{3, 2, 7, 4, 1};
-
-    solve(nums,0,0,0);
-    cout << result;
+    cout << "hello world";
 }
