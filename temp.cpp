427e05c26041b90909645f798cb44676193ea602
junaid7535 <zunaidkhan1492@gmail.com>
2025-07-19 14:40:04 +0530

    commit

--- temp.cpp
+++ temp.cpp
@@ -4,8 +4,31 @@
 #include<map>
 using namespace std;
 
-
-
 int main(){
-    cout << "hello world";
+    string s = "geeksforgeeks";
+    int n = s.size();
+    vector<int>consecutive(s.size());
+    consecutive[0] = 0;
+
+    for(int i = 1; i < s.size(); i++){
+        if(s[i] == s[i-1]){
+            if(consecutive[i-1] > 0){
+                consecutive[i] = consecutive[i-1] + 1;
+            }
+            else{
+                consecutive[i] = 2;
+            }
+        }
+        else{
+            consecutive[i] = 0;
+        }
+    }
+    int k = 2;
+    for(int i = 0; i < n; i++){
+            if(consecutive[i] == k){
+                cout << s.substr(i-k+1,k);
+                s.erase(i - k+1,k);
+            }
+        }
+        //cout << s;
 }
